e28875326f5491741ade20a3d680899d9e6b7580
Jamie Parkinson <jamie.parkinson.12@ucl.ac.uk>
2016-08-29 23:41:22 +0100

    Pretty much React-ively working

---
+++ components/card.jsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import {Card, Label, Icon} from 'stardust';
+import math from 'mathjs';
+import TeX from '../react-components/js/tex.jsx';
+
+import {scaleLinear} from 'd3-scale';
+import {line} from 'd3-shape';
+
+const AlsoKnownAs = ({names}) => (
+  <div className="description also-known">
+    Also known as: {names.join(', ')}
+  </div>
+);
+
+const PDF = ({formula}) => (
+  <Card.Content>
+    <TeX displayMode={true}>{formula}</TeX>
+  </Card.Content>
+);
+
+const Control = (variable) => (
+  <div className="symbol-control">
+    <TeX>{variable.tex}</TeX>
+    <input type="range" min={variable.min}
+                        max={variable.max}
+                        value={variable.value}
+                        step="0.01" 
+                        id={variable.name}
+                        onChange={variable.change}/>
+    <label htmlFor={variable.name}>{variable.value.toFixed(2)}</label>
+  </div>
+)
+
+const Controls = ({variables, values, changeHandler}) => {
+  var controlItems = Object.keys(variables).map(v => 
+    (<Control {...variables[v]}
+              value={parseFloat(values[v])}
+              change={changeHandler.bind(null, v)}
+              key={v} />)
+  );
+  return(
+    <Card.Content>
+      {controlItems}
+    </Card.Content>
+  )
+};
+
+const Statistics = ({values}) => {
+  var valNames = Object.keys(values);
+  var header = valNames.map(v => (<th key={v}>{v}</th>));
+  var texvals = valNames.map(v => (<td key={v}><TeX>{values[v]}</TeX></td>));
+
+  return(
+    <Card.Content>
+      <table className="ui small very compact celled table">
+        <thead>
+          <tr>{header}</tr>
+        </thead>
+        <tbody>
+          <tr>{texvals}</tr>
+        </tbody>
+      </table>
+    </Card.Content>
+  );
+};
+
+class Graph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.width = 290;
+    this.height = 180;
+
+    const xScale = scaleLinear().domain(props.limits.x).range([-5, this.width+5]);
+    const yScale = scaleLinear().domain(props.limits.y).range([this.height - 30, 1]);
+    this.line = line().x(r => xScale(r[0]))
+                      .y(r => yScale(r[1]));
+  }
+
+  lineArr() {
+    var arr = []
+    var xMax = this.props.limits.x[1];
+    var vars = this.props.variables;
+    for (var i = 0; i < 150; i++) {
+      var xv = xMax*i/150;
+      arr[i] = [
+        xv,
+        this.props.func.eval({ ...vars, x: xv })
+      ];
+    }
+    return arr;
+  }
+
+  render() {
+    return (
+      <div className="image svg-wrapper">
+        <svg width={this.width} height={this.height}>
+          <path stroke="black" strokeWidth="4"
+                fill="none" d={this.line(this.lineArr())} />
+        </svg>
+      </div>
+    );
+  }
+}
+
+class DistributionCard extends React.Component {
+  constructor(props) {
+    super(props);
+    // Get initial values
+    var stateToBe = {}
+    var varNames = Object.keys(props.variables);
+    varNames.map(v => {
+      stateToBe = {...stateToBe, [v]: props.variables[v].init};
+    });
+
+    this.state = stateToBe;
+    this.updateVariable = this.updateVariable.bind(this);
+
+    this.pdfFunc = math.compile(props.pdf);
+  }
+
+  updateVariable(name, event) {
+    this.setState({ [name]: event.target.value });
+  }
+
+  render() {
+    return(
+      <Card>
+        <Label corner="right" href={this.props.wiki}>
+          <Icon name="wikipedia" style={{ cursor: 'pointer' }}/>
+        </Label>
+
+        <Card.Content>
+          <div className="header" style={{ fontFamily: 'Montserrat' }}>
+            {this.props.title}
+          </div>
+          <AlsoKnownAs names={this.props.altNames} />
+        </Card.Content>
+
+        <Graph variables={this.state}
+               func={this.pdfFunc}
+               limits={this.props.graphLimits} />
+
+        <PDF formula={this.props.texPdf} />
+
+        <Controls variables={this.props.variables}
+                  values={this.state}
+                  changeHandler={this.updateVariable}/>
+
+        <Statistics values={this.props.statistics} />
+      </Card>
+    );
+  }
+};
+
+export default DistributionCard;
--- index.jsx
+++ index.jsx
@@ -1,29 +1,61 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {Image, Input, Grid} from 'stardust';
 
-import {Rail, Image, Container, Input, Grid} from 'stardust';
+import DistributionCard from './components/card.jsx';
 
 const TopBar = () => (
-	<Grid className="sticky" padded className="topbar">
-		<Grid.Column width={4}>
-		  <Image src='public/probs.svg' className="probs-img"/>
-		  <div className="probs-description">
-		  	Probability distribution explorer.
-		  </div>
-	  </Grid.Column>
-	  <Grid.Column width={12}>
-	  	<Input className="fluid icon right" icon="search red" placeholder="Search for a distribution..." />
-	  </Grid.Column>
+  <Grid className="sticky" padded className="topbar">
+    <Grid.Column width={4}>
+      <Image src='public/probs.svg' className="probs-img"/>
+      <div className="probs-description">
+        Probability distribution explorer
+      </div>
+    </Grid.Column>
+    <Grid.Column width={12}>
+      <Input className="fluid icon right" icon="search red" placeholder="Search for a distribution..." />
+    </Grid.Column>
   </Grid>
 );
 
+const card = {
+  title: 'Normal Distribution',
+  wiki: 'http://en.wikipedia.org/wiki/Normal_distribution',
+  pdf: '(1/sqrt(2*sigma^2*pi))*exp(-(x-mu)^2/(2*sigma^2))',
+  texPdf: 'f(x) = \\frac{1}{\\sqrt{2\\pi\\sigma^2}} \\exp \\left(-\\frac{(x - \\mu)^2}{2 \\sigma^2} \\right)',
+  altNames: ['Gaussian', 'Bell curve'],
+  statistics: {
+    Mean: '\\mu',
+    Median: '\\mu',
+    Mode: '\\mu',
+    Variance: '\\sigma^2'
+  },
+  variables: {
+    mu: { min: 0, max: 6, init: 3.0, tex: '\\mu' },
+    sigma: { min: 0.01, max: 2.0, init: 0.75, tex: '\\sigma' }
+  },
+  graphLimits: {
+    x: [0, 6],
+    y: [0, 0.9]
+  }
+};
+
+const DistributionCards = ({cards}) => {
+  var cardList = cards.map(card => (<DistributionCard {...card} key={card.title} />));
+  return (
+    <div className="ui cards">
+      {cardList}
+    </div>
+  );
+}
+
 const MainContainer = () => (
-	<div>
-		<TopBar/>
-		<Container>
-			Hello
-		</Container>
-	</div>
+  <div>
+    <TopBar/>
+    <div className="card-section-container">
+      <DistributionCards cards={[card]} />
+    </div>
+  </div>
 );
 
 render(<MainContainer />, document.getElementById('app'));
